c7a4cf407e080c4550a951ce467803e705890bcf
古川倖輔 <furukawasaiwaitasuku@furukawasaiwaitasukunoMacBook-Air.local>
2022-09-27 00:38:23 +0900

    JavaScript基本文法

--- a.js
+++ a.js
@@ -1,1 +1,62 @@
-alert("hello world")
+// var hello = "hello tom";
+// var int1 = 1;
+// alert(hello);
+// alert(int1);
+
+// alert(4+9);
+// alert(7-9);
+// alert(9*3);
+// alert(90/5);
+
+// alert("hello"+"world");
+// var str1 = "GO!!!";
+// var str2 = "Tom";
+// alert(str1+str2);
+
+// var orange = 100;
+// var apple = 120;
+// if (orange < apple) {
+// 	alert("みかんの値段がりんごより安い");
+// }
+// else if (orange == apple) {
+// 	alert("みかんとりんごが同じ値段");
+// }
+// else {
+// 	alert(みかんの値段がりんごより高い);
+// }
+
+// var max = 100;
+// var num = 1;
+// var count = 0;
+// while(num < max){
+//   num = num * 3;
+//   count = count + 1;
+// }
+
+// alert('3を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+
+// var i;
+// var num = 0;
+
+// for(i = 1; i < 11; i++){
+//   num = num + i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です');
+
+
+
+alert(1+8);
+alert(8-8);
+alert(7*6);
+alert(345/7);
+
+
+
+
+
+
+
+
+
